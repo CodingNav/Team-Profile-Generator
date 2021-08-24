d43aaf97f783b8a5a3c5c8fa0ab67b93f046fbdb
CodingNav <navleenkaurr@gmail.com>
2021-08-24 16:15:45 -0400

    Added manager bootstrap card to html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const generateManager = (answers) =>
+    `
+<i style="float: right;">${answers.license.badge}</i>
+# ${answers.projectName}
+`;
