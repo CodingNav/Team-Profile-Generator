b30e8654750b97f1fe80000627b779e2348fc74e
CodingNav <navleenkaurr@gmail.com>
2021-08-24 18:40:58 -0400

    Set up intern's inquirer prompts

--- assets/js/index.js
+++ assets/js/index.js
@@ -152,4 +152,31 @@ inquirer
         const engineerContent = generateEngineer(answers);
     });
 
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "What is the intern's name?"
+        },
+        {
+            type: "input",
+            name: "internID",
+            message: "What is the intern's ID?"
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is the intern's email?"
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "What is the intern's school name?"
+        }
+    ])
+    .then((answers) => {
+        const internContent = generateIntern(answers);
+    });
+
     
